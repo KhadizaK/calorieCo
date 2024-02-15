2ca4be3c501fa52dad60d2391007d03fc707c586
KhadizaK <46384611+KhadizaK@users.noreply.github.com>
2024-02-15 17:48:54 -0500

    Add files via upload
    Can now display all usernames in the database

--- storefront/calorieAdmin/models.py
+++ storefront/calorieAdmin/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class calorie(models.model):
-  userInput = models
+class calorie(models.Model):
+  firstName = models.CharField(max_length = 50)
+  title = models.CharField(max_length = 100, default='Default Title')
+
+  def __str__(self):
+    return self.title
--- storefront/calorieAdmin/views.py
+++ storefront/calorieAdmin/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from calorieAdmin.models import calorie
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
 
 def sayHello(request):
-  return render(request, 'home.html', {'name': 'Snow'})
+  calorieDB = calorie()
+  username = request.POST.get('text1', '')
+  calorieDB.firstName = username
+  calorieDB.title = "test1"
+  calorieDB.save()
+  names = calorie.objects.all()
+  # return render(request, 'home.html', {'name': calorie.objects.all()[0].firstName})
+  # return render(request, 'home.html', {'name': username})
+  return render(request, 'home.html', {'names': names, 'username': username})
